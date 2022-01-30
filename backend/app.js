e11e5beb9d4b4e199553a9cd040be732a151aa3e
Navid <navid.anjum@northsouth.edu>
2022-01-30 23:59:38 +0600

    start added

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const express = require("express")
+const app = express()
+
+app.use(express.static("../public"))
+
+app.listen(5000,()=>{
+    console.log("Server listening to port 5000....");
+})
