89a41bc1e71c3b1d83e6ad2543097f92c5cc12df
Navid <navid.anjum@northsouth.edu>
2022-01-31 11:08:16 +0600

    dunno

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 
+const port = process.env.PORT || 5000
+
 app.use(express.static("./public"))
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("Server listening to port 5000....");
 })
